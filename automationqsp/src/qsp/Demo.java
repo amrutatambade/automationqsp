09941dbe02e96fd5e6894f536b2b78236bc64332
amrutatambade <90767517+amrutatambade@users.noreply.github.com>
2021-09-17 09:26:05 +0530

    Update Demo.java

--- automationqsp/src/qsp/Demo.java
+++ automationqsp/src/qsp/Demo.java
@@ -3,7 +3,7 @@ package qsp;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("happy ganesh chaturthi");
+		System.out.println("happy ganesh chaturthi 2021");
 
 	}
 
